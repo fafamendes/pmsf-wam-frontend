1d8bdda939bdfc28e0d18319b498aaaba238d549
Fabrício <fabricio.jmj@gmail.com>
2024-03-07 16:27:54 -0300

    Correção de verificação de tipo de usuário

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,14 +1,17 @@
+import { useTokenContext } from "@context/TokenContext"
 import { useUserContext } from "@context/UserContext"
 import { SearchUsers } from "@layouts/SearchUsers"
 
 export const Home = () => {
 
   const { user } = useUserContext()
+  const { logout } = useTokenContext();
 
   return (
     <div className="flex flex-col items-center justify-center">
       <h1>Home</h1>
-      {user?.role && <SearchUsers />}
+      <button onClick={logout}>Logout</button>
+      {user?.role === 'ADMIN' && <SearchUsers />}
     </div>
   )
 }
