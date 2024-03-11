bd3dceeae34d01fb3d2e81dfcfe26cb243f1919e
Fabrício Mendes <fabricio.jmj@gmail.com>
2024-03-11 07:19:57 -0300

    Inicia implementação do update

--- src/shared/components/user-edit-dialog/UserEditDialog.tsx
+++ src/shared/components/user-edit-dialog/UserEditDialog.tsx
@@ -10,7 +10,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
-import { useCallback, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 
 export interface IUserEditDialogProps {
   children: React.ReactNode;
@@ -21,10 +21,38 @@ export const UserEditDialog: React.FC<IUserEditDialogProps> = ({ children, user 
 
 
   const [switchValue, setSwitchValue] = useState<boolean>(user.status);
+  const inputNameRef = useRef<HTMLInputElement>(null);
+  const inputUsernameRef = useRef<HTMLInputElement>(null);
+  const switchRef = useRef<HTMLButtonElement>(null);
+  const [hasEdited, setHasEdited] = useState<boolean>(false);
 
-  const handleChangeSwitch = useCallback(() => {
-    setSwitchValue(!switchValue)
-  }, [switchValue])
+  const handleChangeSwitch = useCallback((checked: boolean) => {
+    console.log("ESTÁ CHECKADO:", checked)
+    console.log(user.status)
+    setSwitchValue(checked)
+  }, [])
+
+  const handleSaveUser = useCallback((event: React.FormEvent) => {
+    event.preventDefault();
+    if (inputNameRef.current?.value === user.name && inputUsernameRef.current?.value === user.username && switchValue === user.status) {
+      setHasEdited(false);
+      return;
+    } else {
+      setHasEdited(true);
+    }
+    
+  }, [user, switchValue])
+  
+  const handleChangeForm = useCallback((event: React.ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('UHU')
+    if (inputNameRef.current?.value === user.name && inputUsernameRef.current?.value === user.username && switchValue === user.status) {
+      setHasEdited(false);
+      return;
+    } else {
+      setHasEdited(true);
+    }
+  }, [user, switchValue])
 
   return (
     <>
@@ -41,15 +69,14 @@ export const UserEditDialog: React.FC<IUserEditDialogProps> = ({ children, user 
               Matrícula: {user.username}
             </DialogDescription>
           </DialogHeader>
-          <form className="flex flex-col gap-4 items-center" action="
-          ">
-            <Input className="" placeholder="Nome" defaultValue={user.name} />
-            <Input className="" placeholder="Matrícula" defaultValue={user.username} />
+          <form className="flex flex-col gap-4 items-center" onChange={handleChangeForm} onSubmit={handleSaveUser}>
+            <Input ref={inputNameRef} className="" placeholder="Nome" defaultValue={user.name} />
+            <Input ref={inputUsernameRef} className="" placeholder="Matrícula" defaultValue={user.username} />
             <div className="flex gap-4 items-center w-full">
-              <Switch defaultChecked={switchValue} onCheckedChange={handleChangeSwitch} id={'user-status'} />
+              <Switch ref={switchRef} defaultChecked={true} onCheckedChange={handleChangeSwitch} id={'user-status'} />
               <Label htmlFor={'user-status'}>{switchValue ? 'Ativo' : 'Inativo'}</Label>
             </div>
-            <Button className="bg-green-700 w-auto">Salvar</Button>
+            <Button type="submit" disabled={!hasEdited} className="bg-green-700 w-auto">Salvar</Button>
           </form>
         </DialogContent>
       </Dialog>
